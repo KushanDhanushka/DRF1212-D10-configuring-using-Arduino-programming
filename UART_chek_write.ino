348e12744421d71c925bfddffb5b01ee6991ceb1
Kushan Dhanushka <e18075@eng.pdn.ac.lk>
2023-08-14 14:43:45 +0530

    Add files via upload

---
+++ UART_chek_write.ino
@@ -0,0 +1,21 @@
+
+//#include <SPI.h>
+void setup() {
+Serial1.begin(9600); //TX-PA9/RX-PA10
+Serial.begin(9600); //USB (PA11/PA12)
+pinMode(PA5, OUTPUT);
+pinMode(PA6, OUTPUT);
+
+
+digitalWrite(PA5, HIGH);
+digitalWrite(PA6, LOW);
+delay(1000);
+
+}
+
+void loop() {
+byte message[] = {0xFF, 0x56, 0xAE, 0x35, 0xA9, 0x55, 0x90, 0x06, 0x9F, 0x00, 0x03, 0x07, 0x03, 0x00, 0x05 };
+Serial1.write(message, sizeof(message));
+Serial.println("Hi-3");
+delay(1000);
+}
